8b8401c99bbf079f28e0cc3c4de35249589e11cd
kunal528 <64953335+kunal528@users.noreply.github.com>
2023-06-09 16:25:09 +0530

    created footer ui

--- components/Layout.js
+++ components/Layout.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import styles from '../styles/Layout.module.css'
 import Navbar from './Navbar/Navbar'
+import Footer from './Footer/Footer'
 
-const Layout = ({children}) => {
-  return (
-    <div className={styles.app}>
-        <Navbar/>
-        {children}
-    </div>
-  )
+const Layout = ({ children }) => {
+    return (
+        <div>
+            <Navbar />
+            <div className={styles.app}>
+                {children}
+                <Footer />
+            </div>
+        </div>
+    )
 }
 
 export default Layout
