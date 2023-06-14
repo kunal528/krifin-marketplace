4b6a5e64e77875770b8ec34df236aa8d3dcb1132
kunal528 <64953335+kunal528@users.noreply.github.com>
2023-06-14 16:25:13 +0530

    added benefits sections in the home page

--- pages/index.js
+++ pages/index.js
@@ -51,11 +51,52 @@ export default function Home() {
           <div className={styles.buttonOutlined}>More NFTs</div>
         </div>
       </div>
+      <div className={styles.benefitsSection}>
+        <div className={styles.benefitsHeader}>Buy the most interesting web3 Assets  here...</div>
+        <div className={styles.benefits}>
+          <div className={styles.benefit}>
+            <img src="./png/benefit.png" alt="Benefit" className={styles.benefitLogo} />
+            <div>
+              <div className={styles.benefitTitle}>Secure  Investments </div>
+              <div className={styles.benefitDesc}>Non- Market linked returns along with blockchain protection for every transaction.</div>
+            </div>
+          </div>
+          <div className={styles.benefit}>
+            <img src="./png/benefit.png" alt="Benefit" className={styles.benefitLogo} />
+            <div>
+              <div className={styles.benefitTitle}>Stable ROI</div>
+              <div className={styles.benefitDesc}>Earn upto average 11 % pre-tax yields REIT NFT is the most secured Asset-backed NFTs with real-world properties providing APY 11-25%.</div>
+            </div>
+          </div>
+          <div className={styles.benefit}>
+            <img src="./png/benefit.png" alt="Benefit" className={styles.benefitLogo} />
+            <div>
+              <div className={styles.benefitTitle}>Benefits</div>
+              <div className={styles.otherBenefits}>
+                <div>
+                  <div className={styles.otherBenefitsHeader}>Affordable</div>
+                </div>
+                <div>
+                  <div className={styles.otherBenefitsHeader}>24 x 7 Liquidity</div>
+                  <div className={styles.otherBenefitsDesc}>P2P Trade</div>
+                </div>
+                <div>
+                  <div className={styles.otherBenefitsHeader}>Legal Compliance</div>
+                </div>
+                <div>
+                  <div className={styles.otherBenefitsHeader}>Fast Transactions</div>
+                  <div className={styles.otherBenefitsDesc}>Our transactions are executed in T+0 Time Schedule)</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className={styles.partnerOfferSection}>
         <img src="./png/partner-offer.png" alt="Partner" />
-        <div style={{maxWidth: '610px'}}>
+        <div style={{ maxWidth: '610px' }}>
           <div className={styles.partnerOfferHeader}>Want to partner with us ?</div>
-          <div className={styles.description} style={{marginBottom: '30px'}}>We partner with  open-end real estate investment trust focused on managing a portfolio of retail and mixed-use retail community and neighbourhood centres, generally in the mid-market range of $10 to $50 million, from both primary and secondary markets across Dubai, Europe, and United Kingdom.</div>
+          <div className={styles.description} style={{ marginBottom: '30px' }}>We partner with  open-end real estate investment trust focused on managing a portfolio of retail and mixed-use retail community and neighbourhood centres, generally in the mid-market range of $10 to $50 million, from both primary and secondary markets across Dubai, Europe, and United Kingdom.</div>
           <div style={{ display: 'flex', marginBottom: '30px' }}>
             <div className={styles.button}>Connect Us</div>
           </div>
