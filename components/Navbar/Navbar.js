05e658494855bc464432672ce518f829599748b1
ayush572 <ayushdagaeinstein@gmail.com>
2023-07-03 19:11:17 +0530

    made the login and logout functionality. Just a small tweak in the design of the navbar

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,11 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from '../../styles/Navbar.module.css'
 import Link from 'next/link'
 import useWeb3 from '../../lib/useWeb3'
+import useFirebase from '../../lib/useFirebase'
+
 
 const Navbar = () => {
-    const { web3 } = useWeb3();
+    const { account } = useWeb3()
+    let userData;
+    const {login, logout} = useFirebase();
     const [showDropdown, setShowDropdown] = React.useState(false);
+    const [acc, setAcc] = useState(null);
+    useEffect(() => {
+        if (!acc) {
+            userData = JSON.parse(localStorage.getItem('userloggedAcc'));
+            setAcc(userData); // Update the acc state after login
+          console.log("account logged in now:", acc);
+        }
+        else{
+            console.log("account already loggedin:", acc);
+        }
+      }, [acc]);
+      const accountLogin = async () => {
+        try {
+          const res = await login(); // Wait for the login process to complete
+          if(res){
+            userData = JSON.parse(localStorage.getItem('userloggedAcc'));
+            console.log("userData is:", userData);
+            setAcc(userData); // Update the acc state after login
+          }
+        } catch (error) {
+          console.error('Error logging in:', error);
+        }
+      };
+
+      const accountLogout = async () => {
+        try {
+          await logout(); // Wait for the logout process to complete
+          setAcc(null); // Update the acc state after logout
+        } catch (error) {
+          console.error('Error logging out:', error);
+        }
+      }
     return (
         <div className={styles.container}>
             <Link href="/">
@@ -17,10 +53,13 @@ const Navbar = () => {
                 <Link href="/marketplace" className={styles.menuItem}>Trade</Link>
                 {/* <Link href="#" className={styles.menuItem}>Map</Link> */}
                 <Link href="/contact" className={styles.menuItem}>Contact</Link>
-                {web3 ?
-                    <Link href="/profile"><img src={"https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg"} style={{ height: '40px', width: '40px', borderRadius: '75%', objectFit: 'cover', }} alt="your image" /></Link>
-                    : <div className={styles.connectButton}>Connect</div>}
+                {account ?
+                    <div className={styles.connectButton}>{account.toString().substring(0, 3) + '...' + account.toString().substring(account.length -3)}</div>
+                    : <div className={styles.connectButton}>Connect Wallet</div>}
+                {acc ? <Link href="/profile" className={styles.menuItem}><img src={acc.photoURL} height={35} width={35} style={{borderRadius: '100%'}}/></Link>: 
+                <div className={styles.connectButton} onClick={() => { accountLogin() }}>Login</div>}
                 {/* dropdown of chains */}
+                
                 <div className={styles.connectButton} onClick={() => { setShowDropdown(val => !val) }}>
                     <img src='/png/grid.png' height={25} width={25} />
                 </div>
@@ -32,14 +71,7 @@ const Navbar = () => {
                     <Link href="https://www.krifin.in/" target='_blank' className={styles.dropdownItem}>House of Krifin</Link>
                     <Link href="https://www.metaversecouncil.io/" target='_blank' className={styles.dropdownItem}>Metaverse Council</Link>
                     <Link href="/partners" className={styles.dropdownItem}>Partners</Link>
-                    {/* <div className='navbar-dropdown-item'>Socials</div>
-                    <div style={{ display: 'flex', gap: '20px', padding: '10px 25px', flexWrap: 'wrap', justifyContent: 'center' }}>
-                        <Icon name='twitter' style={{ fontSize: '20px' }} className='white' />
-                        <Icon name='instagram' style={{ fontSize: '20px' }} className='white' />
-                        <Icon name='telegram plane' style={{ fontSize: '20px' }} className='white' />
-                        <Icon name='discord' style={{ fontSize: '20px' }} className='white' />
-                        <Icon name='linkedin square' style={{ fontSize: '20px' }} className='white' />
-                    </div> */}
+                    {acc && <div className={styles.dropdownItem} onClick={() => { accountLogout() }}>Logout</div>}
                 </div>
             </div>
         </div>
