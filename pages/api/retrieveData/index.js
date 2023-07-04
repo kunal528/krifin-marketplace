64e775ae03eb2639c6f906b0412d629d76ce1ed2
ayush572 <ayushdagaeinstein@gmail.com>
2023-07-04 21:54:55 +0530

    made some small changes in the navbar

--- pages/api/retrieveData/index.js
+++ pages/api/retrieveData/index.js
@@ -1,6 +1,6 @@
 import { firestore } from "../../../firebaseConfig";
 import {collection, getDocs} from "firebase/firestore";
-import useFirebase from "../../../lib/useFirebase";
+// import useFirebase from "../../../lib/useFirebase";
 
 const retrieveData = async (req, res) => {
   // const {getAllTradeAssets} = useFirebase();
@@ -10,6 +10,7 @@ const retrieveData = async (req, res) => {
     // console.log("snapshot data captured:", snapshot)
     const snapshot = collection(firestore, 'alltradeAssets');
     const dataAll = await getDocs(snapshot);
+    console.log("alldata:", dataAll);
     const data = dataAll.docs.map((doc) => doc.data());
     console.log("data is:", data)
     res.status(200).json(data);
