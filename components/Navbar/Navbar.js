6dbec29c402271c98f7661d6f838c96c112a3a77
ayush572 <ayushdagaeinstein@gmail.com>
2023-07-06 01:16:17 +0530

    invest now page also made adaptable for mobile view

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -111,7 +111,7 @@ const Navbar = () => {
           >
             Metaverse Council
           </Link>
-          <Link href="/partners" className={styles.dropdownItem}>
+          <Link href="/#" className={styles.dropdownItem}>
             Partners
           </Link>
           
