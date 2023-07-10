419891d4ec880afcf853b21b14d5cb879410a6dc
ayush572 <ayushdagaeinstein@gmail.com>
2023-07-11 02:44:51 +0530

    home page made for various sizes

--- components/cards/TopInvestCard.js
+++ components/cards/TopInvestCard.js
@@ -7,11 +7,11 @@ const TopInvestCard = ({no, name, totInvest, image, pnl, pnlVal}) => {
     <div className={styles.investmain}>
         <div className={styles.investcardno}><b>{no}</b></div>
         <div className={styles.investcardimg}>
-            <Image src={image} alt="investcardimg" width={50} height={50} style={{borderRadius: '100%'}}/>
+            <Image src={image} alt="investcardimg" width={50} height={50} style={{borderRadius: '100%'}} className={styles.investcardImg}/>
         </div>
         <div className={styles.content}>
             <div className={styles.investcardname}>{name}</div>
-            <div className={styles.investcardtotinvest}><Image src="/png/white_eth.png" height={25} width={15} /><p style={{color: '#636363', fontWeight: '1000', fontSize :'12px'}}> {totInvest}</p></div>
+            <div className={styles.investcardtotinvest}><Image src="/png/white_eth.png" height={25} width={15} className={styles.whiteEth}/><p className={styles.totalInvest}> {totInvest}</p></div>
         </div>
         <div className={styles.profitorloss} style={pnl ? {color: '#14C8B0'}:{color: '#FF002E'}}>
             <b>{pnl ? "+" : "-" }</b>{pnlVal}
